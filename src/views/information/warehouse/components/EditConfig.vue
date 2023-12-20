09df0f040fe89da8d7454d7f1fb2c42b956ab66c
Chelase <2911996843@qq.com>
2023-12-20 21:13:34 +0800

    仓库配置静态布局完成

--- src/views/information/warehouse/components/EditConfig.vue
+++ src/views/information/warehouse/components/EditConfig.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
 
 const props = defineProps({
   ShowConfig: {
@@ -16,14 +16,64 @@ watch(() => props.ShowConfig, (newValue) => {
   isShow.value = newValue
 })
 
+const IsZone = ref(false)
+const IsTray = ref(false)
+const Disable = ref(false)
+const IsDefault = ref(false)
+
 function closeConfig() {
   emit(['update:OpenConfig'], false)
 }
 </script>
 
 <template>
-  <el-dialog v-model="isShow" title="编辑配置" @close="closeConfig">
-    111
+  <el-dialog v-model="isShow" title="编辑配置" width="35%" @close="closeConfig">
+    <el-row>
+      <span>托盘管理</span>
+      <el-switch
+        v-model="IsZone"
+        size="large"
+        inline-prompt
+        active-text="启用"
+        inactive-text="停用"
+      />
+    </el-row>
+    <el-row>
+      <span>分区管理</span>
+      <el-switch
+        v-model="IsTray"
+        size="large"
+        inline-prompt
+        active-text="启用"
+        inactive-text="停用"
+      />
+    </el-row>
+    <el-row>
+      <span>仓库状态</span>
+      <el-switch
+        v-model="Disable"
+        size="large"
+        inline-prompt
+        active-text="启用"
+        inactive-text="停用"
+      />
+    </el-row>
+    <el-row>
+      <span>默认仓库</span>
+      <el-switch
+        v-model="IsDefault"
+        size="large"
+        inline-prompt
+        active-text="是"
+        inactive-text="否"
+      />
+    </el-row>
+    <template #footer>
+      <el-button>取消</el-button>
+      <el-button type="primary">
+        确定
+      </el-button>
+    </template>
   </el-dialog>
 </template>
 
