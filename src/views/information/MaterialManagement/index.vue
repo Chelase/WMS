07eda0696118ac64792f39412823cca12748f9d1
Loveandmanday <142858342+Loveandmanday@users.noreply.github.com>
2023-12-25 13:22:23 +0800

    1

--- src/views/information/MaterialManagement/index.vue
+++ src/views/information/MaterialManagement/index.vue
@@ -5,15 +5,7 @@
 <template>
   <div>
     <PageMain>
-<<<<<<< HEAD
-<<<<<<< HEAD
       物料管理
-=======
-      物料类型
->>>>>>> lsq-dev
-=======
-      物料管理
->>>>>>> lsq-dev
     </PageMain>
   </div>
 </template>
