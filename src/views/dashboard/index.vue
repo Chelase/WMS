0451a6285995595a1f4ba7f8bbf3c713faa77473
Loveandmanday <142858342+Loveandmanday@users.noreply.github.com>
2024-01-02 09:28:19 +0800

    1

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,41 +1,63 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { 
-  getMaterialStatisticsData,
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+import {
   getInventoryStatistics,
-  getOutboundStatistics
- } from '@/api/modules/dashboard/index.ts'
-//物料统计
+  getMaterialStatisticsData,
+  getOutboundStatistics,
+} from '@/api/modules/dashboard/index.ts'
+
+// 物料统计
 const MaterialStatistics = ref({
   Total: 0,
-  Storage: 0
+  Storage: 0,
 })
-//入库统计
+// 入库统计
 const InventoryStatistics = ref({
   Total: 0,
-  Storage: 0
+  Storage: 0,
 })
 // 出库统计
 const OutboundStatistics = ref({
   Total: 0,
-  Storage: 0
+  Storage: 0,
 })
 
-
-
-const getData = async() => {
-  //物料统计
-  const Mres =  await getMaterialStatisticsData()
+async function getData() {
+  // 物料统计
+  const Mres = await getMaterialStatisticsData()
   MaterialStatistics.value = Mres.Data
-  //入库统计
-  const Ires =  await getInventoryStatistics()
+  // 入库统计
+  const Ires = await getInventoryStatistics()
   InventoryStatistics.value = Ires.Data
-  //出库统计
-  const Ores =  await getOutboundStatistics()
+  // 出库统计
+  const Ores = await getOutboundStatistics()
   OutboundStatistics.value = Ores.Data
 }
+const myChart = ref()
+const dom = ref()
+const option = ref()
+
 onMounted(() => {
   getData()
+  myChart.value = echarts.init(dom.value)
+  option.value = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+      },
+    ],
+  }
+
+  option.value && myChart.value.setOption(option.value)
 })
 </script>
 
@@ -54,10 +76,18 @@ onMounted(() => {
               </div>
             </template>
             <div>
-              <p class="light">物料总数</p>
-              <p class="Btext">{{MaterialStatistics.Total}}</p>
-              <p class="light">当前仓库</p>
-              <p class="Btext">{{MaterialStatistics.Storage}}</p>
+              <p class="light">
+                物料总数
+              </p>
+              <p class="Btext">
+                {{ MaterialStatistics.Total }}
+              </p>
+              <p class="light">
+                当前仓库
+              </p>
+              <p class="Btext">
+                {{ MaterialStatistics.Storage }}
+              </p>
             </div>
           </el-card>
         </el-col>
@@ -72,10 +102,18 @@ onMounted(() => {
               </div>
             </template>
             <div>
-              <p class="light">今日入库</p>
-              <p class="Btext">{{InventoryStatistics.Total}}</p>
-              <p class="light">当前仓库</p>
-              <p class="Btext">{{InventoryStatistics.Storage}}</p>
+              <p class="light">
+                今日入库
+              </p>
+              <p class="Btext">
+                {{ InventoryStatistics.Total }}
+              </p>
+              <p class="light">
+                当前仓库
+              </p>
+              <p class="Btext">
+                {{ InventoryStatistics.Storage }}
+              </p>
             </div>
           </el-card>
         </el-col>
@@ -85,30 +123,46 @@ onMounted(() => {
               <div class="card-header">
                 <span>出库统计</span>
                 <el-button class="button" text>
-                  <svg-icon name="prime:refresh"/>
+                  <svg-icon name="prime:refresh" />
                 </el-button>
               </div>
             </template>
             <div>
-              <p class="light">今日出库</p>
-              <p class="Btext">{{OutboundStatistics.Total}}</p>
-              <p class="light">当前仓库</p>
-              <p class="Btext">{{OutboundStatistics.Storage}}</p>
+              <p class="light">
+                今日出库
+              </p>
+              <p class="Btext">
+                {{ OutboundStatistics.Total }}
+              </p>
+              <p class="light">
+                当前仓库
+              </p>
+              <p class="Btext">
+                {{ OutboundStatistics.Storage }}
+              </p>
             </div>
           </el-card>
         </el-col>
       </el-row>
+      <div ref="dom" class="tupiao" />
     </PageMain>
   </div>
 </template>
 
 <style scoped lang="scss">
-.Btext{
+.Btext {
   font-size: 28px;
 }
-.light{
-  color: rgb(114, 114, 114);
+
+.tupiao {
+  width: 1000px;
+  height: 500px;
 }
+
+.light {
+  color: rgb(114 114 114);
+}
+
 .button {
   float: right;
 }
